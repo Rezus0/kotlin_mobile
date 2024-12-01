4fe37de00463ab706f9d3cd1ae6f70c1a9ef6987
Rezus <github.rzs@yandex.ru>
2024-12-01 21:24:28 +0300

    Лабораторная работа №4(рефакторинг)

--- app/src/main/java/com/example/messengerandroid/model/ChatAdapter.java
+++ app/src/main/java/com/example/messengerandroid/model/ChatAdapter.java
@@ -1,15 +1,12 @@
 package com.example.messengerandroid.model;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.messengerandroid.R;
+import com.example.messengerandroid.databinding.ItemChatBinding;
 
 import java.util.List;
 
@@ -23,19 +20,17 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
 
     @NonNull
     @Override
-    public ChatViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_chat, parent, false);
-        return new ChatViewHolder(view);
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemChatBinding binding = ItemChatBinding.inflate(
+                LayoutInflater.from(parent.getContext()), parent, false
+        );
+        return new ChatViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(ChatViewHolder holder, int position) {
         Chat chat = chatList.get(position);
-        holder.tvSenderName.setText(chat.getSenderName());
-        holder.tvLastMessage.setText(chat.getLastMessage());
-        holder.tvTime.setText(chat.getTime());
-        holder.ivProfile.setImageResource(chat.getProfileImage());
+        holder.bind(chat);
     }
 
     @Override
@@ -44,16 +39,21 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
     }
 
     public static class ChatViewHolder extends RecyclerView.ViewHolder {
-        ImageView ivProfile;
-        TextView tvSenderName, tvLastMessage, tvTime;
 
-        public ChatViewHolder(View itemView) {
-            super(itemView);
-            ivProfile = itemView.findViewById(R.id.ivProfile);
-            tvSenderName = itemView.findViewById(R.id.tvSenderName);
-            tvLastMessage = itemView.findViewById(R.id.tvLastMessage);
-            tvTime = itemView.findViewById(R.id.tvTime);
+        private final ItemChatBinding binding;
+
+        public ChatViewHolder(ItemChatBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
+
+        public void bind(Chat chat) {
+            binding.tvSenderName.setText(chat.getSenderName());
+            binding.tvLastMessage.setText(chat.getLastMessage());
+            binding.tvTime.setText(chat.getTime());
+            binding.ivProfile.setImageResource(chat.getProfileImage());
+        }
+
     }
 }
 
